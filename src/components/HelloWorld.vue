e5c54c8ae63aaf5c65e176c8d1043382e8a60e38
parkjunghwan <19park@recokr.com>
2022-08-03 16:57:37 +0900

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue'
+
+// defineProps<{ msg: string }>()
+
+const recoServices = ref([])
+const recoString = computed(() => recoServices.value.join(","))
+const getRecoServices = () => {
+  recoServices.value = ["admin", "customer", "driver", "mobile"]
+}
+
+onMounted(getRecoServices)
+
+watch(recoServices, () => {
+  console.log("변경됨..")
+})
+</script>
+
+<template>
+  <h1>{{ recoServices }}</h1>
+  <h2>{{ recoString }}</h2>
+</template>
+
+<style scoped>
+</style>
